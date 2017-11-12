09e9d408099ecf3e082920fe6caadd87346a2f77
wongjenn <wongjenn@yahoo.com>
2017-11-12 12:50:02 -0500

    route set up

--- webpack.server.js
+++ webpack.server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base.js');
+const webpackNodeExternals = require('webpack-node-externals');
 
 const config = {
   // Inform webpack that we're building a bundle for nodeJS,
@@ -14,7 +15,8 @@ const config = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build')
-  }
+  },
+  externals: [webpackNodeExternals()]
 };
 
 module.exports = merge(baseConfig, config);
